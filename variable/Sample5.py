f3ea9e0a96b1c015d52dde5b1c26c74b43f53a58
MikanKi <ivyfel21@gmail.com>
2024-05-15 15:17:36 +0900

    variable

---
+++ variable/Sample5.py
@@ -0,0 +1,8 @@
+price = 50
+num = 10
+total = price*num
+print("単価は",price,"円です。")
+print("売上個数は",num,"個です。")
+print("合計金額は",total,"円です。")
+total = total-100
+print("割引すると",total,"円です。")
---
+++ variable/lesson3.py
@@ -0,0 +1,9 @@
+#練習１
+age = int(input("あなたは何歳ですか？"))
+print("あなたは", age, "歳です。")
+
+#練習２
+height = float(input("身長を入力してください。"))
+weight = float(input("体重を入力してください。"))
+print("身長は", height, "センチです。")
+print("体重は", weight, "キロです。")
